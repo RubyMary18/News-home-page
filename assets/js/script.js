dde283acf904702bb8817dc6d0a10d6fc3a04737
RubyMary18 <rubymary18@gmail.com>
2023-11-13 19:08:41 +0530

    removed comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,6 @@ const menuBtn = document.querySelector('.hamburger-menu');
 const closeBtn = document.querySelector('.hamburger-close');
 const nav = document.querySelector('.right-nav');
 const hamburger = document.querySelector('.hamburger');
-console.log(menuBtn);
-
-// hamburger.addEventListener('click', () => {
-//     nav.classList.toggle('show');
-//     if (nav.classList.contains('show')) {
-//         menuBtn.classList.add('hide');
-//         closeBtn.classList.remove('hide');
-//         closeBtn.classList.add('show');
-//     } else if (!nav.classList.contains('hide')) {
-//         closeBtn.classList.remove('hide');
-//         // closeBtn.classList.add('hide');
-//         menuBtn.classList.remove('hide');
-//         menuBtn.classList.add('show');
-//     }
-// })
 
 menuBtn.addEventListener('click', () => {
     if(nav.classList.contains('hide')){
@@ -24,7 +9,6 @@ menuBtn.addEventListener('click', () => {
     }
     nav.classList.add('show');
     if(nav.classList.contains('show')){
-        // menuBtn.classList.toggle('hide');
         menuBtn.classList.remove('show');
         menuBtn.classList.add('hide');
     }
@@ -33,14 +17,11 @@ menuBtn.addEventListener('click', () => {
 })
 
 closeBtn.addEventListener('click', () => {
-    // nav.classList.toggle('hide');
     nav.classList.remove('show');
     nav.classList.add('hide');
     if (nav.classList.contains('hide')) {
-        // closeBtn.classList.toggle('hide');
         closeBtn.classList.remove('show');
         closeBtn.classList.add('hide');
-        // menuBtn.classList.toggle('show');
         menuBtn.classList.remove('hide');
         menuBtn.classList.add('show');
     }
